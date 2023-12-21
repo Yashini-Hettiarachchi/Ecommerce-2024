7e104273488f7070e4678ba66113d476e2420df6
Yashini Hettiarachchi <99383400+IT21181474@users.noreply.github.com>
2023-12-21 17:31:39 +0530

    Updated project

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 import express from 'express';
 import colors from "colors";
 import dotenv from "dotenv";
+import morgan from 'morgan'
+import connctDB from './config/db.js';
 
 
 //configure env
 dotenv.config();
 
+//database config
+connctDB();
+
 
 //rest object
 const app = express();
+
+//middlewares
+app.use(express.json()); 
+app.use(morgan('dev')); //log request to console
 
 //rest api
 app.get('/', (req, res) => {
